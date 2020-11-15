3f428b49168cba1727bf79d0d3d6d26880a3486d
salbisser105 <41337754+salbisser105@users.noreply.github.com>
2020-11-15 14:54:40 -0500

    Add files via upload
    Cambios reglas basicas// procesamiento funciones

--- src/public/js/metodos/punto_fijo.js
+++ src/public/js/metodos/punto_fijo.js
@@ -1,14 +1,11 @@
-function funcionUnoNormal(x) {
-    return Math.log((Math.sin(x) * Math.sin(x)) + 1) - (1 / 2)-(x);
-}
-function funcionG(x) {
-    return Math.log((Math.sin(x) * Math.sin(x)) + 1) - (1 / 2);
-}
 
 function puntoFijo(){
     
     const table = require('table').table;
     const fs = require('fs');
+    var mathjs = require('mathjs');
+    let strf = 'log(sin(x)^2+1)-(1/2)-(x)';
+    let strg = 'log(sin(x)^2+1)-(1/2)';
     let data = [
         ['iter', 'xi', 'g(xi)','f(xi)', 'E']
     ];
@@ -17,16 +14,24 @@ function puntoFijo(){
     niter = 100;
     contador = 0;
     var error = tol+1;
-    fx= funcionUnoNormal(x0);
+    fx= mathjs.evaluate(strf,{x:x0});
+    if(niter < 0){
+        console.log("El numero de iteraciones debe ser positivo");
+        return("Error");
+    }
+    if(tol < 0){
+        console.log("La tolerancia debe ser positiva");
+        return("Error");
+    }
 
-    data.push([contador, Number.parseFloat(x0).toPrecision(17), Number.parseFloat(funcionG(x0)).toPrecision(17),Number.parseFloat(fx).toExponential(1), '']);
+    data.push([contador, Number.parseFloat(x0).toPrecision(17), Number.parseFloat(mathjs.evaluate(strg,{x:x0})).toPrecision(17),Number.parseFloat(fx).toExponential(1), '']);
     while((fx!=0) && (error>tol )&& (contador<niter)){
-        xn= funcionG(x0);
-        fx= funcionUnoNormal(xn);
+        xn= mathjs.evaluate(strg,{x:x0});
+        fx= mathjs.evaluate(strf,{x:xn});
         error= Math.abs(xn-x0);
         x0= xn;
         contador= contador+1;
-        data.push([contador, Number.parseFloat(x0).toPrecision(17), Number.parseFloat(funcionG(x0)).toPrecision(17),Number.parseFloat(fx).toExponential(1),Number.parseFloat(error).toExponential(1)]);
+        data.push([contador, Number.parseFloat(x0).toPrecision(17), Number.parseFloat(mathjs.evaluate(strg,{x:x0})).toPrecision(17),Number.parseFloat(fx).toExponential(1),Number.parseFloat(error).toExponential(1)]);
     }
     if (fx == 0) {
         console.log("se encontro una aproximación en: " +x0);
