1ce2206d4f8a48286e1d1efd528f9f5843515c55
jplealj <37347503+jplealj@users.noreply.github.com>
2020-11-15 13:03:42 -0500

    Add files via upload

--- src/public/js/metodos/incrementales.js
+++ src/public/js/metodos/incrementales.js
@@ -1,20 +1,21 @@
-function f(x){
-    return Math.log((Math.sin(x) * Math.sin(x)) + 1) - (1/2);
-}
-
-
 function incrementales(){
+    var mathjs = require('mathjs');
+    str = 'log(sin(x)^2 + 1) - (1/2)';
     x0 = -3;
     delta = 0.5;
     niter = 100;
-    fx0 = f(x0);
+    if(niter < 0){
+        console.log("El numero de iteraciones debe ser positivo");
+        return("Error");
+    }
+    fx0 = mathjs.evaluate(str,{x:x0});
     if (fx0 == 0){
         console.log(x0 + " es una raiz")
     }
     else {
         x1 = x0 + delta;
         cont = 0;
-        fx1 = f(x1);
+        fx1 = mathjs.evaluate(str,{x:x1});
         while (cont < niter){
             if (fx0 * fx1 < 0){
                 if (fx1 == 0){
@@ -27,7 +28,7 @@ function incrementales(){
             x0 = x1;
             fx0 = fx1;
             x1 = x0 + delta;
-            fx1 = f(x1);
+            fx1 = mathjs.evaluate(str,{x:x1});
             cont = cont + 1;
         }
     }
--- src/public/js/metodos/raicesMultiples.js
+++ src/public/js/metodos/raicesMultiples.js
@@ -1,35 +1,35 @@
-function h(x){
-    return (Math.pow(Math.E, x)) - (x) - (1);
-}
-
-function dh(x){
-    return (Math.pow(Math.E, x)) - (1);
-}
-
-function ddh(x){
-    return (Math.pow(Math.E, x));
-}
-
 function raicesMultiples(){
     const table = require('table').table;
+    var mathjs = require('mathjs');
+    let str = 'e^x - x - 1';
     const fs = require('fs');
+    var dh = mathjs.derivative(str,'x');
+    var ddh = mathjs.derivative(dh, 'x');
     let data = [
         ['Iter', 'xi', 'f(xi)', 'E']
     ];
     x0 = 1;
     tol = 0.0000001; //E-7
     niter = 100;
-    fx = h(x0);
-    dfx = dh(x0);
-    ddfx = ddh(x0);
+    if(niter < 0){
+        console.log("El numero de iteraciones debe ser positivo");
+        return("Error");
+    }
+    if(tol < 0){
+        console.log("La tolerancia debe ser positiva");
+        return("Error");
+    }
+    fx = mathjs.evaluate(str,{x:x0});
+    dfx = dh.evaluate({x:x0});
+    ddfx = ddh.evaluate({x:x0});
     cont = 0;
     error = tol + 1;
     data.push([cont, Number.parseFloat(x0).toPrecision(17), Number.parseFloat(fx).toExponential(1),'']);
-    while ((error > tol) && (cont < niter) && (fx != 0) && (dfx != 0)){
+    while ((error > tol) && (cont < niter)){
         x1 = x0 - ((fx * dfx)/((Math.pow(dfx, 2)) - (fx * ddfx)));
-        fx = h(x1);
-        dfx = dh(x1);
-        ddfx = ddh(x1);
+        fx = mathjs.evaluate(str,{x:x1});
+        dfx = dh.evaluate({x:x1});
+        ddfx = ddh.evaluate({x:x1});
         error = Math.abs(x1 - x0);
         x0 = x1;
         cont = cont + 1;
